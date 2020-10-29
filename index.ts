9c978ee1c6a570ddb526d070d15b17883c5fe868
admin <admin@huelite.in>
2020-10-29 23:05:44 +0530

    added types --hue user --hue device --hue timer --hue ldb

--- index.ts
+++ index.ts
@@ -1,3 +1,39 @@
-export const type = {
-    test: 49
+export type HUE_USER_t = {
+    id: string,
+    userName?: string,
+    email: string,
+    fbId?: string,
+    googleId?: string
+    devices?: HUE_DEVICE_t[]
+}
+
+export type HUE_DEVICE_t = {
+    id: string,
+    Mac: string,
+    IP?: string,
+    deviceName?: string,
+    groupName?: string,
+    lastState?: string,
+    timers?: HUE_TIMER_t[]
+    user?: HUE_USER_t
+    //add timers to data type timers
+}
+
+export type HUE_TIMER_t = {
+    id: string,
+    device?: HUE_DEVICE_t,
+    HR?: number,
+    MIN?: number,
+    DT?: number,
+    ET?: number,
+    DAYS?: number,
+    ldb: HUE_LDB_t
+    //TODO add LDB to data type timers
+}
+
+export type HUE_LDB_t = {
+    id: string,
+    TS: number,
+    DST: number,
+    DBS: number,
 }
