c737990ef5b2934c9cb60841d0aa980942f3d6ea
root <root@103-211-217-123.webhostbox.net>
2020-11-01 09:57:50 +0000

    changes in server side envirenment

--- src/router/auth/googleAuth/config.ts
+++ src/router/auth/googleAuth/config.ts
@@ -3,9 +3,9 @@
 module.exports = {
 
     'googleAuth': {
-        'clientID': '226637505045-n06bfsc2qg3k2ndcgqe8m4i2cg54g7de.apps.googleusercontent.com',
-        'clientSecret': 'UTj2DLPGNtrNsIeHjPhgTYwV',
-        'callbackURL': 'http://localhost:4000/backend/auth/google/redirect'
+        'clientID': '226637505045-in210mbrcpkbv549q5q8o5oco3n8aj0e.apps.googleusercontent.com',
+        'clientSecret': 'Y3YkUayl0nDJcx-Qri9V8--_',
+        'callbackURL': 'https://www.huelite.in/backend/auth/google/redirect/'
     }
 
 };
--- src/router/auth/googleAuth/index.ts
+++ src/router/auth/googleAuth/index.ts
@@ -4,11 +4,13 @@ export var googleAuthRouter = express.Router();
 const googleStatergy = require("./googleStatergy")
 const http = require('http');
 const url = require('url');
-
+var deeplink = require('node-deeplink');
+const path = require('path');
 
 function redirect(response: any, url: any) {
     response.writeHead(302, {
         Location: url,
+        host: "hueiteapp://"
     });
     response.end();
 }
@@ -16,7 +18,7 @@ function redirect(response: any, url: any) {
 googleAuthRouter.get("/", passport.authenticate('google', { scope: ['profile', 'email'] }))
 
 googleAuthRouter.get("/redirect", async (req: any, res: any) => {
-    return res.send("test google auth")
+    return redirect(res, "googleLoginRedirect")
 })
 
 googleAuthRouter.get("/proxy", async (req: any, res: any) => {
@@ -28,5 +30,18 @@ googleAuthRouter.get("/proxy", async (req: any, res: any) => {
         return res.redirect(decodeURIComponent(parameters.authServiceUrl));
     }
     // redirect response from the auth service to your application
-    return redirect(res, "hueite_app://")
+    console.log(">> redirecting to app")
+    return res.status(301).redirect("/deeplink")
+})
+
+googleAuthRouter.get("/deeplink", deeplink({
+    fallback: 'https://www.huelite.in/backend/auth/google',
+    android_package_name: 'com.abhi6676.node',
+    ios_store_link:
+        'https://itunes.apple.com/us/app/cups-unlimited-coffee/id556462755?mt=8&uo=4'
+}))
+
+googleAuthRouter.get("/deeplinktest", async (req: any, res: any) => {
+    console.log("deeplink test" + path.join(__dirname + '/index.html'))
+    return res.sendFile(path.join(__dirname + '/index.html'));
 })
