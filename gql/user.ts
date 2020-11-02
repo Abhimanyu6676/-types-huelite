903a37d4657c964caf1034b52892e2b0cd2b0a93
admin <admin@huelite.in>
2020-11-02 21:29:54 +0530

    --@types added

--- gql/user.ts
+++ gql/user.ts
@@ -1,36 +1,46 @@
+const ldb_fields = `id
+TS
+DST
+DBS`
+
+const timer_fields = `id
+HR
+MIN
+DAYS
+DT
+ET
+ldb{
+  ${ldb_fields}
+}`
+
+const device_fields = `id
+deviceName
+Mac
+groupName
+lastState
+IP
+timers{
+  ${timer_fields}
+}`
+
+const user_fields = ` id
+userName
+email
+fbId
+googleId
+devices{
+  ${device_fields}
+}`
+
+
 export const gql_getUserWithFbId = (`query(
     $fbId:String!
   ){
     allUsers(where:{fbId:$fbId}, first:1){
-      id
-      userName
-      email
-      fbId
-      googleId
-      devices{
-        id
-        deviceName
-        Mac
-        groupName
-        lastState
-        IP
-        timers{
-          id
-          HR
-          MIN
-          DAYS
-          DT
-          ET
-          ldb{
-            id
-            TS
-            DST
-            DBS
-          }
-        }
-      }
+      ${user_fields}
     }
   }`)
+
 
 export const gql_createUser = (`mutation(
     $userName:String,
@@ -53,3 +63,13 @@ export const gql_createUser = (`mutation(
       googleId
     }
   }`)
+
+
+export const query_findUserWithEmail = (`query(
+  $email:String!
+){
+  allUsers(where:{email:$email}, first:1){
+    ${user_fields}
+  }
+}`)
+
