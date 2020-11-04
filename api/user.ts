84a35b534ae0ff45cb400eabb8198b09b1260767
admin <admin@huelite.in>
2020-11-04 16:39:31 +0530

    -- added backend types in small case and Device local types in capital case
    --added helper function for conversion
      *hueContainer backend-to-local
      *hueDevice backend-to-Local

--- api/user.ts
+++ api/user.ts
@@ -1,7 +1,7 @@
-import { HUE_USER_t } from "../index";
+import { HUE_User_t } from "../index";
 
 export interface API_USER_SIGNIN_rs_t {
     success: boolean
     error?: string
-    data?: HUE_USER_t
+    data?: HUE_User_t
 }
--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-
-
 export enum HUE_DEVICE_CONTAINER_TYPE_e {
     SINGLETON = 1,
     MULTIPLE = 0,
@@ -13,39 +11,60 @@ export enum HUE_GROUP_CATEGORIES_e {
     STRIP_NEO = 4,
 }
 
+/** @description >- Client side Local representation of user Object */
 export type HUE_USER_t = {
-    id: string | undefined,
+    id?: string,
     userName?: string,
     email: string,
     fbId?: string,
     googleId?: string
-    containers?: HUE_Container_t[]
+    containers?: HUE_CONTAINER_t[]
 }
 
-export type HUE_Container_t = {
-    groupName: string;
-    groupUUID: string;
-    groupAdmin: string;
-    activeMode: string;
-    conType: HUE_DEVICE_CONTAINER_TYPE_e;
-    conCategory: HUE_GROUP_CATEGORIES_e;
-    timers: HUE_TIMER_group_t[];
-    devices: HUE_Device_t[];
-};
+/** @description >- backend representation of user Object */
+export type HUE_User_t = {
+    id: string,
+    userName?: string,
+    email: string,
+    fbId?: string,
+    googleId?: string
+    containers: HUE_Container_t[]
+}
 
+/** @description >- Client side Local representation of container Object */
 export type HUE_CONTAINER_t = {
+    id?: string,
     groupName: string;
     groupUUID: string;
     groupAdmin: string;
     activeMode: string;
     conType: HUE_DEVICE_CONTAINER_TYPE_e;
     conCategory: HUE_GROUP_CATEGORIES_e;
-    timers: HUE_TIMER_group_t[];
+    timers?: HUE_TIMER_group_t[];
     devices: HUE_DEVICE_t[];
 };
 
+/** @description >- backend representation of conatiner Object */
+export type HUE_Container_t = {
+    id: string,
+    groupName: string;
+    groupUUID: string;
+    groupAdmin: string;
+    activeMode: string;
+    conType: HUE_DEVICE_CONTAINER_TYPE_e;
+    conCategory: HUE_GROUP_CATEGORIES_e;
+    timers?: HUE_TIMER_group_t[];
+    devices: HUE_Device_t[];
+    user?: HUE_User_t
+};
+
+/** @description >- Client side Local representation of device Object */
+export type HUE_DEVICE_t = (HUE_Device_t & { socket: any, id?: string, })
+
+
+/** @description >- backend representation of device Object */
 export type HUE_Device_t = {
-    id: string | undefined,
+    id: string,
     Mac: string,
     IP?: string,
     deviceName?: string,
@@ -55,25 +74,44 @@ export type HUE_Device_t = {
     user?: HUE_USER_t
     //add timers to data type timers
 }
-export type HUE_DEVICE_t = (HUE_Device_t & { socket: any })
-
 
 export type HUE_TIMER_group_t = HUE_TIMER_t & { devices: string[] }
 
+/** @description >- Client side Local representation of timer Object */
 export type HUE_TIMER_t = {
-    id: string | undefined,
-    device?: HUE_Device_t,
+    id?: string,
     HR?: number,
     MIN?: number,
     DT?: number,
     ET?: number,
     DAYS?: number,
     ldb: HUE_LDB_t
+}
+
+/** @description >- backend representation of timer Object */
+export type HUE_Timer_t = {
+    id: string,
+    device?: HUE_Device_t,
+    HR?: number,
+    MIN?: number,
+    DT?: number,
+    ET?: number,
+    DAYS?: number,
+    ldb: HUE_Ldb_t
     //TODO add LDB to data type timers
 }
 
+/** @description >- Client side Local representation of LDB Object */
 export type HUE_LDB_t = {
-    id: string | undefined,
+    id?: string,
+    TS: number,
+    DST: number,
+    DBS: number,
+}
+
+/** @description >- backend representation of LDB Object */
+export type HUE_Ldb_t = {
+    id: string,
     TS: number,
     DST: number,
     DBS: number,
