22e26ae9844a0317dc2d722650d24fc6601f99be
admin <admin@huelite.in>
2021-01-07 21:25:22 +0530

    Squashed '@types/huelite/' changes from 6cddd4d..6503620
    6503620 comminting types changes
    acdd798 Merge commit 'e381b003ddf1fdd6f2636c6124bb928927ee3f7c'
    7452eb1 Merge branch 'server-1.1' of https://github.com/Abhimanyu6676/HUEliteServer into server-1.1
    f9603e7 sdg
    3877b3f Merge branch 'master' of https://github.com/Abhimanyu6676/HUElite_App_Expo
    2768d12 feat(USER SCREEN(login/signup)): containers removed from database
    cdea721 refactor(database(container, device)): containers removed from database
    30938d7 refactor(database(container, device)): containers removed from database
    
    git-subtree-dir: @types/huelite
    git-subtree-split: 6503620ce5ff0274dfecaed1925251bb13ba249f

---
+++ device/gql.ts
@@ -0,0 +1,34 @@
+import { HUE_User_fields_no_devices } from "../user/userGqlFieldsWithNoDevices"
+
+
+
+export const HUE_Device_fields_compactUserWithNoDevices = `id
+Hostname
+deviceName
+Mac
+IP
+ssid
+hsv
+groupName
+lastState
+timers{
+    id
+}
+user{
+    ${HUE_User_fields_no_devices}
+}`
+
+
+
+export const HUE_Device_fields_noUser = `id
+Hostname
+deviceName
+Mac
+IP
+ssid
+hsv
+groupName
+lastState
+timers{
+    id
+}`
